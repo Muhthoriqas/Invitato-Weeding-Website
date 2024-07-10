911da51e83eb6c623eaec6fbcf754c01f757b96c
Muhthoriqas <muhthoriqas1719@gmail.com>
2024-07-10 10:28:27 +0800

    init react & chara ui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Text } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <></>
+      <Text fontSize="md">
+        I'm using a custom font-size value for this text
+      </Text>
+    </>
+  );
+}
+
+export default App;
