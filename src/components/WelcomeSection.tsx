1b0ed00858df41e1fbd5161c81011409eb85dc95
Muhthoriqas <muhthoriqas1719@gmail.com>
2024-07-12 22:28:28 +0800

    add welcome section

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -1,23 +1,128 @@
-import theme from '../theme';
-import { Flex, ChakraProvider } from '@chakra-ui/react';
-import LeftDesktopSection from './welcome/LeftDesktopSection';
-import RightSection from './welcome/RightSection';
+import { Box, Button, Text } from '@chakra-ui/react';
+import HeadingPoppins from './heading/HeadingPoppins';
+import { useEffect, useState } from 'react';
+import HeadingButlerLight from './heading/HeadingButlerLight';
+import HeadingNewsreaderItalic from './heading/HeadingNewsreaderItalic';
+
+const images = [
+  '../src/assets/images/Slideshow/Slideshow-Cover-1.jpg',
+  '../src/assets/images/Slideshow/Slideshow-Cover-2.jpg',
+  '../src/assets/images/Slideshow/Slideshow-Cover-3.jpg',
+  '../src/assets/images/Slideshow/Slideshow-Cover-4.jpg',
+  '../src/assets/images/Slideshow/Slideshow-Cover-5.jpg',
+];
 
 const WelcomeSection = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <>
-      <ChakraProvider theme={theme}>
-        <Flex
-          height="100vh"
-          direction="row"
-          width={'100%'}
-          color={'alternativeColorText'}
+    <Box height="100vh" position="relative" overflow="hidden">
+      <Box className="slide">
+        {images.map((image, index) => (
+          <Box
+            key={index}
+            className={`css-${index}`}
+            backgroundImage={`linear-gradient(rgba(50, 48, 48, 0.5), rgba(50, 48, 48, 0.5)), url(${image})`}
+            backgroundSize="cover"
+            backgroundPosition="center"
+            height="100%"
+            width="100%"
+            top="0"
+            left="0"
+            position="absolute"
+            transition="opacity 1s ease-in-out"
+            style={{
+              opacity: index === currentImageIndex ? 1 : 0,
+            }}
+          ></Box>
+        ))}
+      </Box>
+      <Box
+        flex="none"
+        width={{ base: '100%', md: '500px' }}
+        bgSize="cover"
+        bgPosition="center"
+        color="alternativeColorText"
+        p={8}
+        height="100vh"
+        padding="2rem"
+        position="relative"
+      >
+        <HeadingPoppins text="WEDDING ANNOUNCEMENT" margin="60px 0 8px 0" />
+        <Box
+          display="flex"
+          justifyContent="center"
+          height="calc(90% - 130px)"
+          alignItems="center"
+          flexDirection="column"
         >
-          <LeftDesktopSection />
-          <RightSection />
-        </Flex>
-      </ChakraProvider>
-    </>
+          <Box textAlign="center" marginTop="47px">
+            <HeadingButlerLight text="TIFFANY & JARED" />
+            <HeadingNewsreaderItalic text="#TImetoshaRE" />
+          </Box>
+        </Box>
+        <Button
+          appearance="none"
+          userSelect="none"
+          position="relative"
+          whiteSpace="nowrap"
+          verticalAlign="middle"
+          outline="transparent solid 2px"
+          outlineOffset="2px"
+          lineHeight="1.2"
+          borderRadius="md"
+          fontWeight="semibold"
+          transitionProperty="common"
+          transitionDuration="normal"
+          height="10"
+          minWidth="10"
+          fontSize="md"
+          paddingInlineStart="4"
+          paddingInlineEnd="4"
+          background="transparent"
+          marginLeft="auto"
+          padding="0px"
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="center"
+          marginTop="4rem"
+          color="alternativeColorText"
+          _hover={{ background: 'transparent' }}
+        >
+          <Text
+            fontFamily="poppins"
+            textTransform="uppercase"
+            marginRight="7px"
+            fontSize="sm"
+          >
+            Scroll To Begining
+          </Text>
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 448 512"
+            height="17"
+            width="17"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
+          </svg>
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
