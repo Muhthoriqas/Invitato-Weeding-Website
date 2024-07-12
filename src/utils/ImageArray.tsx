e0b6c1bfbfa2e179ae19f6145f084b52dd03146e
Muhthoriqas <muhthoriqas1719@gmail.com>
2024-07-13 00:41:17 +0800

    final update path images

--- src/utils/ImageArray.tsx
+++ src/utils/ImageArray.tsx
@@ -1,9 +1,9 @@
 const images = [
-  '../../public/Slideshow/Slideshow-Cover-1.jpg',
-  '../../public/Slideshow/Slideshow-Cover-2.jpg',
-  '../../public/Slideshow/Slideshow-Cover-3.jpg',
-  '../../public/Slideshow/Slideshow-Cover-4.jpg',
-  '../../public/Slideshow/Slideshow-Cover-5.jpg',
+  '/Slideshow/Slideshow-Cover-1.jpg',
+  '/Slideshow/Slideshow-Cover-2.jpg',
+  '/Slideshow/Slideshow-Cover-3.jpg',
+  '/Slideshow/Slideshow-Cover-4.jpg',
+  '/Slideshow/Slideshow-Cover-5.jpg',
 ];
 
 export default images;
