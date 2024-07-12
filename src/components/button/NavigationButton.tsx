668f25750778b19b0c38339c0b5cf676163b6e25
Muhthoriqas <muhthoriqas1719@gmail.com>
2024-07-13 04:09:05 +0800

    finalize - add navbar & deploy

--- src/components/button/NavigationButton.tsx
+++ src/components/button/NavigationButton.tsx
@@ -1,12 +1,23 @@
 import { useState } from 'react';
-import { Button } from '@chakra-ui/react';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Text,
+  Divider,
+  Heading,
+  Box,
+} from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '../../assets/icons/Icon';
-const NavigationButton = () => {
-  const [isClick, setIsClick] = useState(false);
 
-  const handleClick = () => {
-    setIsClick((prev) => !prev);
-  };
+const NavigationButton = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
@@ -17,10 +28,108 @@ const NavigationButton = () => {
         boxShadow={'xl'}
         outline={'transparent solid 2px'}
         color={'gray.800'}
-        onClick={handleClick}
+        onClick={isOpen ? onClose : onOpen}
       >
-        {isClick ? <CloseIcon /> : <HamburgerIcon />}
+        {isOpen ? <CloseIcon /> : <HamburgerIcon />}
       </Button>
+
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent
+          padding={'24px 16px'}
+          maxWidth={{ base: '300px', md: '480px' }}
+          width={'100%'}
+          textAlign={'right'}
+        >
+          <DrawerCloseButton />
+          <DrawerHeader>
+            <Heading
+              fontFamily={'newsreader_italic'}
+              fontSize={'4xl'}
+              fontWeight={'300'}
+              fontStyle={'italic'}
+              marginTop={'16px'}
+              color={'secondaryColorText'}
+            >
+              #TImetoshaRE
+            </Heading>
+          </DrawerHeader>
+
+          <DrawerBody fontFamily={'newsreader_normal'}>
+            <Box padding={'15px 0px'}>
+              <Text
+                fontSize={'sm'}
+                textTransform={'uppercase'}
+                cursor={'pointer'}
+                letterSpacing={'3px'}
+                color={'secondaryColorText'}
+              >
+                Bride & Groom
+              </Text>
+              <Box
+                as="hr"
+                opacity="0.6"
+                border="0"
+                borderBottom="1px solid"
+                borderColor="secondaryColorText"
+                width="100%"
+              />
+            </Box>
+            <Box padding={'15px 0px'}>
+              <Text
+                fontSize={'sm'}
+                textTransform={'uppercase'}
+                cursor={'pointer'}
+                letterSpacing={'3px'}
+                color={'secondaryColorText'}
+              >
+                Live Streaming
+              </Text>
+              <Box
+                as="hr"
+                opacity="0.6"
+                border="0"
+                borderBottom="1px solid"
+                borderColor="secondaryColorText"
+                width="100%"
+              />
+            </Box>
+            <Box padding={'15px 0px'}>
+              <Text
+                fontSize={'sm'}
+                textTransform={'uppercase'}
+                cursor={'pointer'}
+                letterSpacing={'3px'}
+                color={'secondaryColorText'}
+              >
+                Wedding Gift
+              </Text>
+              <Box
+                as="hr"
+                opacity="0.6"
+                border="0"
+                borderBottom="1px solid"
+                borderColor="secondaryColorText"
+                width="100%"
+              />
+            </Box>
+          </DrawerBody>
+
+          <DrawerFooter
+            color={'secondaryColorText'}
+            fontSize={'xs'}
+            fontFamily={'newsreader_normal'}
+            display={'flex'}
+            justifyContent={'flex-end'}
+            alignItems={'center'}
+          >
+            <Box textAlign={'center'}>
+              <Text>Created with Love by Invitato</Text>
+              <Text>2024 Tiffany & Jared. All Rights Reserved</Text>
+            </Box>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
