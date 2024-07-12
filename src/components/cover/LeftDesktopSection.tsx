8659b110cac1a198c68869780309e197f48340a6
Muhthoriqas <muhthoriqas1719@gmail.com>
2024-07-12 22:27:51 +0800

    update header butler and left desktop section

---
+++ src/components/cover/LeftDesktopSection.tsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Text,
+  useBreakpointValue,
+  VStack,
+  Heading,
+} from '@chakra-ui/react';
+
+import { Fade } from 'react-awesome-reveal';
+import HeadingPoppins from '../heading/HeadingPoppins';
+import HeadingButlerLight from '../heading/HeadingButlerLight';
+const LeftDesktopSection = () => {
+  const displayLeftSection = useBreakpointValue({ base: 'none', md: 'block' });
+
+  return (
+    <Box
+      position="fixed"
+      left="0"
+      top="0"
+      height="100vh"
+      width={{ base: '100%', md: 'calc(100% - 500px)' }}
+      borderRightWidth="8px"
+      borderRightStyle="solid"
+      borderRightColor="blackAlpha.400"
+      bgImage="linear-gradient(rgba(50, 48, 48, 0.5), rgba(50, 48, 48, 0.5)), url('../src/assets/images/Left-Desktop.jpg')"
+      bgSize="cover"
+      bgPosition="center"
+      color="alternativeColorText"
+      padding="42px"
+      display={displayLeftSection}
+      zIndex="10"
+      overflow="hidden"
+    >
+      <VStack align="start" spacing={0}>
+        <Fade duration={1000} direction="up" triggerOnce>
+          <HeadingPoppins text="WEDDING ANNOUNCEMENT" margin="2px 0 0 0" />
+        </Fade>
+        <Box
+          paddingTop="32px"
+          letterSpacing="4px"
+          textTransform="uppercase"
+          color="alternativeColorText"
+        >
+          <HeadingButlerLight fontSize="7xl" lineHeight="1" text="TIFFANY &" />
+          <HeadingButlerLight fontSize="7xl" text="JARED" />
+        </Box>
+        <Fade duration={1000} direction="up" triggerOnce>
+          <Text
+            maxW="800px"
+            letterSpacing="1px"
+            fontFamily="newsreader_italic"
+            fontStyle="italic"
+            marginTop="18px"
+          >
+            "Aku ingin mencintaimu dengan sederhana; dengan kata yang tak sempat
+            diucapkan kayu kepada api yang menjadikannya abu. Aku ingin
+            mencintaimu dengan sederhana; dengan isyarat yang tak sempat
+            disampaikan awan kepada hujan yang menjadikannya tiada."
+            <br />— Sapardi Djoko Damono
+          </Text>
+        </Fade>
+      </VStack>
+    </Box>
+  );
+};
+
+export default LeftDesktopSection;
--- src/components/heading/HeadingButlerLight.tsx
+++ src/components/heading/HeadingButlerLight.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 interface HeadingButlerLightProps {
   margin?: string;
   padding?: string;
+  fontSize?: string;
+  lineHeight?: string;
   text: string;
 }
 
@@ -11,16 +13,19 @@ const HeadingButlerLight: React.FC<HeadingButlerLightProps> = ({
   margin,
   padding,
   text,
+  fontSize,
+  lineHeight,
 }) => {
   return (
     <>
       <Heading
         as="h2"
         fontWeight={'300'}
-        fontSize={'4xl'}
+        fontSize={`${fontSize ? fontSize : '4xl'}`}
         fontFamily="butler_light"
         margin={margin}
         padding={padding}
+        lineHeight={lineHeight}
       >
         {text}
       </Heading>
